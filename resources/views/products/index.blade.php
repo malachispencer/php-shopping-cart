eb859a1771b45742f25692ee523b453e34e57c09
Malachi Spencer <m.spencer@live.co.uk>
2021-03-20 20:45:05 +0000

    Added form to add products to cart

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Products</title>
 </head>
 <body>
   <h1>Products</h1>
@@ -13,6 +13,20 @@
     <div>
       {{ $product->name }}
     </div>
+    <div>
+      {{ "£{$product->price}" }}
+    </div>
+    <form action="/cart" method="POST">
+      <select name="quantity">
+        @for($i = 1; $i <= $product->in_stock; $i++) {
+          <option value="<?php echo $i ?>">{{ $i }}</option>
+        }
+        @endfor
+      </select>
+      <input type="hidden" value="<?php echo $product->id ?>">
+      <input type="submit" value="Add To Cart">
+      </form>
+    <br>
   @endforeach
 </body>
 </html>
