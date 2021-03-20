3448a34e907ae8e08685eb6637e4157b4c0812c3
Malachi Spencer <m.spencer@live.co.uk>
2021-03-20 21:19:04 +0000

    Added routes for adding products and viewing cart

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartController;
 
 Route::get('/', [ProductController::class, 'index']);
+
+Route::get('/cart', [CartController::class, 'index']);
+Route::post('/cart', [CartController::class, 'add']);
